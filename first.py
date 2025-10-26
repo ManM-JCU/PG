6d5829e279b75047fa3656772b2c016631086fa9
ManM-JCU <manmar00@jcu.cz>
2025-10-26 14:30:59 +0000

    Úkol 1

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+# Zadat číslo
+cislo = int(input("Zadej číslo: "))
+
+# Zjistíme zda je číslo sudé nebo liché
+if cislo % 2 == 0:
+    print(f"Číslo {cislo} je sudé.")
+else:
+    print (f"Číslo {cislo} je liché.")
